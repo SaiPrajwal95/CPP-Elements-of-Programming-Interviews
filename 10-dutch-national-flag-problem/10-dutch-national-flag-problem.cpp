54cf6ecb7f5785caa883e58e1fe4d9c5a7326b8e
prajwal <prajwal@automotus.co>
2019-04-01 09:45:00 -0700

    Add EPI book solution to the Dutch Flag Sort problem

--- 10-dutch-national-flag-problem/10-dutch-national-flag-problem.cpp
+++ 10-dutch-national-flag-problem/10-dutch-national-flag-problem.cpp
@@ -23,7 +23,7 @@ void swap (int& num1, int& num2) {
  *  @param Arr Reference to the input array that has to be dutch sorted
  *  @param pivotIndex Index for the pivot element
  */ 
-void dutchNationalFlagSort (std::vector<int>& Arr, const int& pivotIndex) {
+void dutchNationalFlagSortPJ (std::vector<int>& Arr, const int& pivotIndex) {
     int lower = 0, higher = Arr.size()-1;
     int pivotVal = Arr[pivotIndex];
     int repeatTracker = 0; // Keeps track of number of sorted pivot values.
@@ -52,16 +52,50 @@ void dutchNationalFlagSort (std::vector<int>& Arr, const int& pivotIndex) {
     }
 }
 
+/** @brief dutchSortFromBook Sorts the Array as described in the comments
+ *                           at the top of the file
+ *  @param inputArr Reference to the input array that has to be dutch sorted
+ *  @param pivotIndex Index for the pivot element
+ */ 
+void dutchSortFromBook (std::vector<int>& inputArr, const int& pivotIndex) {
+    int pivotVal = inputArr[pivotIndex];
+    int lower = 0, middle = 0, higher = inputArr.size(), iterationNum = 0;
+    while (middle < higher) {
+        if (inputArr[middle] < pivotVal) {
+            swap (inputArr[middle++], inputArr[lower++]);
+        } else if (inputArr[middle] == pivotVal) {
+            ++middle;
+        } else {
+            swap (inputArr[middle], inputArr[--higher]);
+        }
+        
+        // This section prints the state of array for each step
+        std::cout<<"Step "<< ++iterationNum << " :";
+        for ( const int& elem : inputArr) {
+            std::cout << elem << " ";
+        }
+        std::cout<<"\n";
+    }
+}
+
 int main()
 {
     std::vector<int> inputArray = {4,6,9,3,2,1,4,0,0,6,4,3,2};
+    std::vector<int> inputArrayCopy = {4,6,9,3,2,1,4,0,0,6,4,3,2};
     int pivotIndex;
     std::cout << "Enter the pivot index: ";
     std::cin >> pivotIndex;
-    std::cout << "Sorting the array in Dutch flag format...\n";
-    dutchNationalFlagSort(inputArray, pivotIndex);
+    std::cout << "Sorting the array in Dutch flag format (PJ Version) ...\n";
+    dutchNationalFlagSortPJ (inputArray, pivotIndex);
     std::cout << "\nResultant array is as follows:\n";
     for ( const int& elem : inputArray) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+    std::cout << "Sorting the array in Dutch flag format (EPI Book Version) ...\n";
+    dutchSortFromBook (inputArrayCopy, pivotIndex);
+    std::cout << "\nResultant array is as follows:\n";
+    for ( const int& elem : inputArrayCopy) {
         std::cout << elem << " ";
     }
     return 0;
